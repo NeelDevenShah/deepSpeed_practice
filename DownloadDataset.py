79b919a13116c8d8fe5f623f001032a03b2763d8
Neel Shah <neeldevenshah@gmail.com>
2023-10-18 18:39:11 +0530

    Chnaged the path according to the excess error

--- DownloadDataset.py
+++ DownloadDataset.py
@@ -1,7 +1,7 @@
 import torchvision.datasets as tvd
 
 # Specify the path where you want to save the MNIST dataset
-download_path = "/MNIST"
+download_path = "/nlsasfs/home/gpucbh/vyakti1/neel_tmp/code/deepSpeed_practice/MNIST"
 
 # Download the MNIST dataset to the specified path
 tvd.MNIST(download_path, train=True, download=True)
